3011c105d2129b495985dd990fa49e3e7b191016
Rezowana Akter <rezowana@noobyia.email>
2022-03-14 17:06:24 +0600

    Update main.php

--- minitube/frontend/views/layouts/main.php
+++ minitube/frontend/views/layouts/main.php
@@ -43,14 +43,21 @@ AppAsset::register($this);
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
+        $menuItems[] = [
+            'label' => 'Logout ('.Yii::$app->user->identity->username.')',
+            'url'   => ['/site/logout'],
+            'linkOptions' => [
+                'data-method' => 'post'
+            ],
+        ];
+            // '<li>'
+            // . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
+            // . Html::submitButton(
+            //     'Logout (' . Yii::$app->user->identity->username . ')',
+            //     ['class' => 'btn btn-link logout']
+            // )
+            // . Html::endForm()
+            // . '</li>';
     }
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav ml-auto'],
